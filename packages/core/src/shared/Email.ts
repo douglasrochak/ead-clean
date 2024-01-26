fa3888c5149a1844e184402cd2a250f81b1d1b4b
Douglas R <douglas.srochak@gmail.com>
2024-01-26 15:47:08 -0300

    refactor(core): Refactor Email class constructor and getters

--- packages/core/src/shared/Email.ts
+++ packages/core/src/shared/Email.ts
@@ -2,22 +2,22 @@ import Validator from "../../src/shared/Validator"
 
 export default class Email {
   constructor(
-    readonly value?: string,
+    readonly value: string = "",
     attribute?: string,
     object?: string,
   ) {
-    this.value = value?.trim() ?? ""
+    this.value = value.trim()
     Validator.value(this.value, attribute, object)
     .email()
     .throwIfError()
   }
 
   get username(): string {
-    return this.value!.split("@")[0]!
+    return this.value.split("@")[0]!
   }
 
   get domain(): string {
-    return this.value!.split("@")[1]!
+    return this.value.split("@")[1]!
   }
 
   static isValid(email: string): boolean {
