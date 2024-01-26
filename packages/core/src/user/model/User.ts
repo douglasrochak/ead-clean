bfc3e35b2a1e29513787c040d5357589138751b0
Douglas R <douglas.srochak@gmail.com>
2024-01-26 15:53:50 -0300

    refactor(core): Add properties and constructor to User model

--- packages/core/src/user/model/User.ts
+++ packages/core/src/user/model/User.ts
@@ -1,4 +1,7 @@
+import Email from "../../shared/Email";
 import Entity, { EntityProps } from "../../shared/Entity";
+import PasswordHash from "../../shared/PasswordHash";
+import UserName from "../../shared/UserName";
 
 export interface UserProps extends EntityProps{
   name?: string;
@@ -8,5 +11,17 @@ export interface UserProps extends EntityProps{
 }
 
 export default class User extends Entity<User, UserProps> {
+  readonly name: UserName;
+  readonly email: Email;
+  readonly password: PasswordHash | null;
+  readonly admin: boolean;
 
+  constructor(props: UserProps) {
+    super(props);
+
+    this.name = new UserName(props.name!);
+    this.email = new Email(props.email)
+    this.password = props.password ? new PasswordHash(props.password) : null
+    this.admin = props.admin ?? false
+  }
 }
