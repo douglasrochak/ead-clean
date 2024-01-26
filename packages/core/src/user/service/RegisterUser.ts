3e7c3d9d1ce6066c9f8412478fcdca94945b186a
Douglas R <douglas.srochak@gmail.com>
2024-01-26 15:56:05 -0300

    feat(core): Add RegisterUser service implementation

---
+++ packages/core/src/user/service/RegisterUser.ts
@@ -0,0 +1,35 @@
+import { User } from "..";
+import StrongPassword from "../../shared/StrongPassword";
+import UseCase from "../../shared/UseCase";
+import Validator from "../../shared/Validator";
+import CryptoProvider from "../provider/CryptoProvider";
+import RepositoryUser from "../provider/RepositoryUser";
+
+export type Input = {
+  name: string;
+  email: string;
+  password: string;
+  admin?: boolean;
+}
+
+export default class RegisterUser implements UseCase<Input, void> {
+
+  constructor(
+    private readonly repository: RepositoryUser,
+    private readonly cryptoProvider: CryptoProvider,
+  ) { }
+
+  async execute(input: Input): Promise<void> {
+    const { name, email, password } = input
+    const strongPassword = new StrongPassword(password)
+    const cryptoPassword = this.cryptoProvider.crypto(strongPassword.value)
+
+    const userAlreadyExists = await this.repository.findByEmail(email)
+    Validator.value(userAlreadyExists)
+    .null("USER_ALREADY_EXISTS")
+    .throwIfError()
+
+    const newUser = new User({name, email, password: cryptoPassword})
+    await this.repository.save(newUser)
+  }
+}
