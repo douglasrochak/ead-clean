94bd95f1fa5d924aae1b9a970778d1bf4403f9a1
Douglas R <douglas.srochak@gmail.com>
2024-01-26 13:14:11 -0300

    feat(core): create Email class to handle emails

---
+++ packages/core/src/shared/Email.ts
@@ -0,0 +1,26 @@
+import Validator from "../../src/shared/Validator"
+
+export default class Email {
+  constructor(
+    readonly value?: string,
+    attribute?: string,
+    object?: string,
+  ) {
+    this.value = value?.trim() ?? ""
+    Validator.value(this.value, attribute, object)
+    .email()
+    .throwIfError()
+  }
+
+  get username(): string {
+    return this.value!.split("@")[0]!
+  }
+
+  get domain(): string {
+    return this.value!.split("@")[1]!
+  }
+
+  static isValid(email: string): boolean {
+    return Validator.value(email).email().valid
+  }
+}
