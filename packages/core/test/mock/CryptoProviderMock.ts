363f00566c86f9b30114ab3865a6eb2d373f6ed6
Douglas R <douglas.srochak@gmail.com>
2024-01-27 12:24:02 -0300

    test(core): Update CryptoProviderMock's method name from crypto to encrypt

--- packages/core/test/mock/CryptoProviderMock.ts
+++ packages/core/test/mock/CryptoProviderMock.ts
@@ -1,7 +1,7 @@
 import { CryptoProvider } from "../../src"
 
 export default class CryptoProviderMock implements CryptoProvider {
-    crypto(_: string): string {
+    encrypt(_: string): string {
         return "$2a$12$2Wn08lE/gzq9VihLoMSVbe7fdAoCOMg6uVE3RQaJnEJc5Wa7eXuly"
     }
     compare(senha: string, _: string): boolean {
