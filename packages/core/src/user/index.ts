08a456c51d770be41bc7b8349b0aad10fe108540
Douglas R <douglas.srochak@gmail.com>
2024-01-27 12:22:24 -0300

    refactor(core): Add UserLogin service to exports

--- packages/core/src/user/index.ts
+++ packages/core/src/user/index.ts
@@ -2,7 +2,8 @@ import User, { UserProps } from "./model/User";
 import UserRepository from "./provider/UserRepository";
 import CryptoProvider from "./provider/CryptoProvider";
 import RegisterUser from "./service/RegisterUser";
+import UserLogin from "./service/UserLogin";
 
 export type { UserProps, UserRepository, CryptoProvider }
 
-export { User, RegisterUser}
+export { User, RegisterUser, UserLogin}
