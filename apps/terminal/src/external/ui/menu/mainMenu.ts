680993ee85dd5f9470831035cadef878aba0ce4f
Douglas R <douglas.srochak@gmail.com>
2024-01-27 17:31:31 -0300

    feat(terminal): Add user login functionality to main menu

--- apps/terminal/src/external/ui/menu/mainMenu.ts
+++ apps/terminal/src/external/ui/menu/mainMenu.ts
@@ -1,14 +1,18 @@
+import { loginUser } from "../user/loginUser";
 import { registerUser } from "../user/registerUser";
 import Terminal from "../util/Terminal";
 
 export default async function mainMenu() {
 
   while(true) {
-    const [_, response] = await Terminal.menu("Main Menu", ["User Register", "Exit"])
+    const [_, response] = await Terminal.menu("Main Menu", ["User Register", "User Login", "Exit"])
 
     switch(response) {
       case "User Register":
         await registerUser()
+        break
+      case "User Login":
+        await loginUser()
         break
       case "Exit":
         process.exit(0)
