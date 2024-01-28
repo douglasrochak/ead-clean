d21f0369abb920a1b6b6da36fc4275d288f10428
Douglas R <douglas.srochak@gmail.com>
2024-01-28 02:24:33 -0300

    feat(backend): Authentication and user registration endpoints

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello World!')
+import dotenv from 'dotenv';
+dotenv.config();
+
+import app from './external/api/config';
+
+import UserRepositoryPrisma from './external/db/UserRepositoryPrisma';
+import BcryptCryptoProvider from './external/auth/BcryptCryptoProvider';
+import JWTProvider from './external/auth/JWTProvider';
+import RegisterUserController from './adapters/RegisterUserController';
+import LoginUserController from './adapters/LoginUserController';
+
+// ------------- Dependency
+
+if(!process.env.JWT_SECRET) throw new Error('JWT_SECRET not found')
+const jwtProvider = new JWTProvider(process.env.JWT_SECRET)
+const userRepository = new UserRepositoryPrisma()
+const cryptoProvider = new BcryptCryptoProvider()
+
+// ------------- Open Routes
+new RegisterUserController(app, userRepository, cryptoProvider)
+
+// ------------- Close Routes
+new LoginUserController(app, userRepository, cryptoProvider, jwtProvider)
