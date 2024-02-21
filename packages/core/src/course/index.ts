16d345d13e669b928c97b4540d0ac3fbf3ace552
Douglas R <douglas.srochak@gmail.com>
2024-02-21 18:04:29 -0300

    chore(core): Add AddLesson service to Course index

--- packages/core/src/course/index.ts
+++ packages/core/src/course/index.ts
@@ -4,8 +4,11 @@ import Lesson, { LessonProps } from './model/Lesson';
 import Lessons from './model/Lessons';
 
 import CourseRepository from './provider/CourseRepository';
+
+// Services
 import SaveCourse from './service/SaveCourse';
+import AddLesson from './service/AddLesson';
 
 export type { CourseProps, CourseRepository, LessonProps }
 
-export { Course, SaveCourse, Lesson, Lessons }
+export { Course, SaveCourse, AddLesson, Lesson, Lessons }
